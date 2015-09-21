a2d23cdcae8423caa240a85984971c336fe4997b
osmantle <mluintel@gmail.com>
2015-09-20 23:21:08 -0400

    third commit

--- app/src/main/java/com/sojhomanchhe/app/constitutionofnepal_2072/SplashScreen.java
+++ app/src/main/java/com/sojhomanchhe/app/constitutionofnepal_2072/SplashScreen.java
@@ -1,7 +1,35 @@
 package com.sojhomanchhe.app.constitutionofnepal_2072;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
 /**
  * Created by mantle on 9/20/15.
  */
-public class SplashScreen {
+public class SplashScreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally {
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        finish();
+    }
 }
